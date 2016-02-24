4407d9a08267f6e6b47e5c972e77167130787f96
Dyjesse18 <dyjesse18@saintjosephhs.com>
2016-02-24 08:39:03 -0500

    Update BeatBox.java

--- BeatBox.java
+++ BeatBox.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.swing.*;
+import java.io.*;
+
 
 //this program will have:
 //6 buttons
@@ -13,6 +15,12 @@ public class Beatbox extends JFrame implements ActionListener {
         private static final long serialVersionUID = 1;
         private JPanel mainPanel;
         private JPanel buttonPanel;
+        private boolean sound1 = false;
+        private boolean sound2 = false;
+        private boolean sound3 = false;
+        private boolean sound4 = false;
+        private boolean sound5 = false;
+        private boolean sound6 = false;
         
         public LayoutTest() {
                 
@@ -83,16 +91,22 @@ public class Beatbox extends JFrame implements ActionListener {
                 //button actions 
                 switch(button.getActionCommand() ) {
                         case " One ":
+                                sound1 = true;
                                 break;
                         case " Two ":
+                                sound2 = true;
                                 break;
                         case " Three ":
+                                sound3 = true;
                                 break;
                         case " Four ":
+                                sound4 = true;
                                 break;
                         case " Five ":
+                                sound5 = true;
                                 break;
                         case " Six ":
+                                sound6 = true;
                                 break;
                         default:
                                 JOptionPane.showMessageDialog( this, "WHERE ARE YOU! THIS DOESNT EXIST!OH GOD HOW DID YOU GET HERE" );
@@ -103,8 +117,199 @@ public class Beatbox extends JFrame implements ActionListener {
         
         public static void main(String[] args) {
                 
-            new LayoutTest();
-                
+                new LayoutTest();
+                if(sound1==true)
+                {
+                        try
+                        {
+                                WavFile wavFile = WavFile.openWavFile(new File(args[0]));
+                                int framesRead;
+                                double min = Double.MAX_VALUE;
+                                double max = Double.MIN_VALUE;
+                                wavFile.display();
+                                int numChannels = wavFile.getNumChannels();
+                                double[] buffer = new double[100 * numChannels];
+                        
+                        do
+                        {
+                                framesRead = wavFile.readFrames(buffer, 100);
+                                for (int s=0 ; s<framesRead * numChannels ; s++)
+                                { 
+                                        if (buffer[s] > max) max = buffer[s];
+                                        if (buffer[s] < min) min = buffer[s];
+                                }//end of for loop
+                                
+                        }//end of do
+                        while (framesRead != 0);
+                        {
+                                wavFile.close();
+                        }//while end
+                        catch (Exception r)
+                        {
+                                System.err.println(r);
+                        }//catch end
+                        
+                }//end of sound1
+                if(sound2==true)
+                {
+                        try
+                        {
+                                WavFile wavFile = WavFile.openWavFile(new File(args[0]));
+                                int framesRead;
+                                double min = Double.MAX_VALUE;
+                                double max = Double.MIN_VALUE;
+                                wavFile.display();
+                                int numChannels = wavFile.getNumChannels();
+                                double[] buffer = new double[100 * numChannels];
+                        
+                        do
+                        {
+                                framesRead = wavFile.readFrames(buffer, 100);
+                                for (int s=0 ; s<framesRead * numChannels ; s++)
+                                { 
+                                        if (buffer[s] > max) max = buffer[s];
+                                        if (buffer[s] < min) min = buffer[s];
+                                }//end of for loop
+                                
+                        }//end of do
+                        while (framesRead != 0);
+                        {
+                                wavFile.close();
+                        }//while end
+                        catch (Exception r)
+                        {
+                                System.err.println(r);
+                        }//catch end
+                        
+                }//end of sound2
+                if(sound3==true)
+                {
+                        try
+                        {
+                                WavFile wavFile = WavFile.openWavFile(new File(args[0]));
+                                int framesRead;
+                                double min = Double.MAX_VALUE;
+                                double max = Double.MIN_VALUE;
+                                wavFile.display();
+                                int numChannels = wavFile.getNumChannels();
+                                double[] buffer = new double[100 * numChannels];
+                        
+                        do
+                        {
+                                framesRead = wavFile.readFrames(buffer, 100);
+                                for (int s=0 ; s<framesRead * numChannels ; s++)
+                                { 
+                                        if (buffer[s] > max) max = buffer[s];
+                                        if (buffer[s] < min) min = buffer[s];
+                                }//end of for loop
+                                
+                        }//end of do
+                        while (framesRead != 0);
+                        {
+                                wavFile.close();
+                        }//while end
+                        catch (Exception r)
+                        {
+                                System.err.println(r);
+                        }//catch end
+                        
+                }//end of sound3
+                if(sound4==true)
+                {
+                        try
+                        {
+                                WavFile wavFile = WavFile.openWavFile(new File(args[0]));
+                                int framesRead;
+                                double min = Double.MAX_VALUE;
+                                double max = Double.MIN_VALUE;
+                                wavFile.display();
+                                int numChannels = wavFile.getNumChannels();
+                                double[] buffer = new double[100 * numChannels];
+                        
+                        do
+                        {
+                                framesRead = wavFile.readFrames(buffer, 100);
+                                for (int s=0 ; s<framesRead * numChannels ; s++)
+                                { 
+                                        if (buffer[s] > max) max = buffer[s];
+                                        if (buffer[s] < min) min = buffer[s];
+                                }//end of for loop
+                                
+                        }//end of do
+                        while (framesRead != 0);
+                        {
+                                wavFile.close();
+                        }//while end
+                        catch (Exception r)
+                        {
+                                System.err.println(r);
+                        }//catch end
+                        
+                }//end of sound4
+                if(sound5==true)
+                {
+                        try
+                        {
+                                WavFile wavFile = WavFile.openWavFile(new File(args[0]));
+                                int framesRead;
+                                double min = Double.MAX_VALUE;
+                                double max = Double.MIN_VALUE;
+                                wavFile.display();
+                                int numChannels = wavFile.getNumChannels();
+                                double[] buffer = new double[100 * numChannels];
+                        
+                        do
+                        {
+                                framesRead = wavFile.readFrames(buffer, 100);
+                                for (int s=0 ; s<framesRead * numChannels ; s++)
+                                { 
+                                        if (buffer[s] > max) max = buffer[s];
+                                        if (buffer[s] < min) min = buffer[s];
+                                }//end of for loop
+                                
+                        }//end of do
+                        while (framesRead != 0);
+                        {
+                                wavFile.close();
+                        }//while end
+                        catch (Exception r)
+                        {
+                                System.err.println(r);
+                        }//catch end
+                        
+                }//end of sound5
+                if(sound6==true)
+                {
+                        try
+                        {
+                                WavFile wavFile = WavFile.openWavFile(new File(args[0]));
+                                int framesRead;
+                                double min = Double.MAX_VALUE;
+                                double max = Double.MIN_VALUE;
+                                wavFile.display();
+                                int numChannels = wavFile.getNumChannels();
+                                double[] buffer = new double[100 * numChannels];
+                        
+                        do
+                        {
+                                framesRead = wavFile.readFrames(buffer, 100);
+                                for (int s=0 ; s<framesRead * numChannels ; s++)
+                                { 
+                                        if (buffer[s] > max) max = buffer[s];
+                                        if (buffer[s] < min) min = buffer[s];
+                                }//end of for loop
+                                
+                        }//end of do
+                        while (framesRead != 0);
+                        {
+                                wavFile.close();
+                        }//while end
+                        catch (Exception r)
+                        {
+                                System.err.println(r);
+                        }//catch end
+                        
+                }//end of sound6
         }//main method end
 
   }//end of program
