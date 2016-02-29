1bfc952eeca83384deec765963ec31f0c726c8f2
Dyjesse18 <dyjesse18@saintjosephhs.com>
2016-02-29 08:20:41 -0500

    Create BeatBox2.java

---
+++ BeatBox2.java
@@ -0,0 +1,140 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.swing.*;
+import java.io.*;
+import java.lang.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
+
+
+//this program will have:
+//6 buttons
+//1 soundbutton panel
+//play sounds from the windows system
+
+public class Beatbox extends JFrame implements ActionListener {
+        
+        // Private variables
+        private static final long serialVersionUID = 1;
+        private JPanel mainPanel;
+        private JPanel buttonPanel;
+        
+        public LayoutTest() {
+                
+                //constructor
+                super( "BeatBox" );
+                JButton button;
+                JLable label;
+                
+                //allows  window to be closed
+                setDefaultClose Operation(JFrame.EXIT_ON_CLOSE);
+                setLayout( new BorderLayout() );
+                
+                //creates the button
+                buttonPanel = new JPanel();
+                buttonPanel.setLayout( new GridLayout(1 , 6) );
+                
+                //creates button 1
+                button = new JButton( "Sound1" );
+                button.setActionCommand( " One " );
+                button.addActionCommand( this );
+                buttonPanel.add( button );
+                
+                //creates button 2
+                button = new JButton( "Sound2" );
+                button.setActionCommand( " Two " );
+                button.addActionCommand( this );
+                buttonPanel.add( button );
+                
+                //creates button 3
+                button = new JButton( "Sound3" );
+                button.setActionCommand( " Three " );
+                button.addActionCommand( this );
+                buttonPanel.add( button );
+                
+                //creates button 4
+                button = new JButton( "Sound4" );
+                button.setActionCommand( " Four " );
+                button.addActionCommand( this );
+                buttonPanel.add( button );
+                
+                //creates button 5
+                button = new JButton( "Sound5" );
+                button.setActionCommand( " Five " );
+                button.addActionCommand( this );
+                buttonPanel.add( button );
+                
+                //creates button 6
+                button = new JButton( "Sound6" );
+                button.setActionCommand( " Six " );
+                button.addActionCommand( this );
+                buttonPanel.add( button );
+                
+                //adds buttons
+                add(buttonPanel, BorderLayout.SOUTH);
+
+                //sizes the window to the prefered size
+                setSize( getPreferredSize() );
+                //packs all the components together
+                pack();
+                //makes the window visible
+                setVisible( true );
+                
+        }// layout test end
+        
+        public void  actionPerformed( SctionEvent e ){
+                JButton button = (JButton)e.getSource();
+                
+                //button actions 
+                switch(button.getActionCommand() ) {
+                        case " One ":
+                                fileName= "Alarm01.wav";
+                                break;
+                        case " Two ":
+                                fileName = "Alarm02.wav";
+                                break;
+                        case " Three ":
+                                fileName = "tada.wav";
+                                break;
+                        case " Four ":
+                                fileName = "Alarm03.wav";
+                                break;
+                        case " Five ":
+                                fileName = "Alarm04.wav";
+                                break;
+                        case " Six ":
+                                fileName = "notify.wav";
+                                break;
+                        default:
+                                JOptionPane.showMessageDialog( this, "WHERE ARE YOU! THIS DOESNT EXIST!OH GOD HOW DID YOU GET HERE" );
+                                break;
+                }//end of switch
+                
+        }//end of action performed
+public static void mian(String[] args)
+{
+        File soundFile = new File("This PC/OS(C:)/Windows//media/" + fileName);
+        AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
+        
+        DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
+        Clip clip = (Clip) AudioSystem.getLine(info);
+        clip.open(sound);
+        
+        clip.addLineListener(new LineListener() 
+        {
+                public void update(LineEvent event) 
+                {
+                        if (event.getType() == LineEvent.Type.STOP)
+                        {
+                                event.getLine().close();
+                                System.exit(0);
+                        }//if end
+                }//update end
+        });//end of clip listener
+        clip.start();
+}//end of main
+      
