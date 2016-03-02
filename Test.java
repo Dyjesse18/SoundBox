609feff4a6c93e90f1967c0f2750b87e79bca133
Dyjesse18 <dyjesse18@saintjosephhs.com>
2016-03-02 08:43:48 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -14,8 +14,12 @@ public class Test extends JFrame implements ActionListener {
         private JPanel mainPanel;
         private JPanel buttonPanel;
         private JPanel buttonPanel1;
+        private JPanel buttonPanel2;
         public double EnemyHP = 30;
         public double YourHP = 30;
+        Random Ran = new Random();
+        int x = Ran.nextInt(1, 2, 3, 4);
+
         
         
         public LayoutTest() {
@@ -24,11 +28,40 @@ public class Test extends JFrame implements ActionListener {
                 super( "Test" );
                 JButton button;
                 JButton button1;
+                JButton button2
                 JLable label;
+                
+                JFrame myFrame = new JFrame("Pokemon ripoff");
+                myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                myFrame.setVisible( true );
+                myFrame.setLayout( new GridLayout( 2, 2, 50, 10 ) );
+                layout = new GridBagLayout();
+                myFrame.setLayout( layout );
                 
                 //allows  window to be closed
                 setDefaultClose Operation(JFrame.EXIT_ON_CLOSE);
                 setLayout( new BorderLayout() );
+                
+                GridBagConstraints c = new GridBagConstraints();
+                // general constraints
+                c.fill = GridBagConstraints.BOTH;
+        
+                // label constraints
+                c.weightx = 0.0;
+                c.gridwidth = GridBagConstraints.RELATIVE;
+        
+                
+                myLabel2= new JLable("Your hp:" + YourHP);
+                myLabel2.setForeground(Color.BLUE);
+                myLabel2.setFont(new Font("TimesRoman", Font.BOLD, 24));
+                layout.setConstraints( myLabel2, c );
+                myFrame.add(myLabel2);
+                
+                myLabel= new JLable("Enemy hp:" + EnemyHP);
+                myLabel.setForeground(Color.RED);
+                myLabel.setFont(new Font("TimesRoman", Font.BOLD, 24));
+                layout.setConstraints( myLabel, c );
+                myFrame.add(myLabel);
                 
                 //creates the button
                 buttonPanel = new JPanel();
@@ -41,13 +74,13 @@ public class Test extends JFrame implements ActionListener {
                 buttonPanel.add( button );
                 
                 //creates button 2
-                button = new JButton( "RUN" );
+                button = new JButton( "ITEMS" );
                 button.setActionCommand( " Two " );
                 button.addActionCommand( this );
                 buttonPanel.add( button );
                 
                 //creates button 3
-                button = new JButton( "ITEMS" );
+                button = new JButton( "RUN" );
                 button.setActionCommand( " Three " );
                 button.addActionCommand( this );
                 buttonPanel.add( button );
@@ -67,10 +100,31 @@ public class Test extends JFrame implements ActionListener {
                 button1.addActionCommand( this );
                 buttonPanel1.add( button1 );
                 
-                button1 = new JButton( "back" );
+                button1 = new JButton( "BACK" );
                 button1.setActionCommand( " 3 " );
                 button1.addActionCommand( this );
                 buttonPanel1.add( button1 );
+                
+                //adds buttons
+                add(buttonPanel1, BorderLayout.SOUTH);
+                
+                //creates button2 a
+                button2 = new JButton( "POTION" );
+                button2.setActionCommand( " a " );
+                button2.addActionCommand( this );
+                buttonPanel2.add( button2 );
+                
+                //creates button2 B
+                button2 = new JButton( "ROCK" );
+                button2.setActionCommand( " b " );
+                button2.addActionCommand( this );
+                buttonPanel2.add( button2 );
+                
+                //creates button2 c
+                button2 = new JButton( "BACK" );
+                button2.setActionCommand( " c " );
+                button2.addActionCommand( this );
+                buttonPanel2.add( button2 );
                 
                 //sizes the window to the prefered size
                 setSize( getPreferredSize() );
@@ -81,39 +135,42 @@ public class Test extends JFrame implements ActionListener {
                 
         }// layout test end
         
-        public void  actionPerformed( SctionEvent e ){
+        public void  actionPerformed( ActionEvent e ){
                 JButton button = (JButton)e.getSource();
                 
                 //button actions 
                 switch(button.getActionCommand() ) {
                         case " One ":
                                 buttonPanel.setvisible(false);
+                                buttonPanel1.setvisble(true);
                                 break;
                         case " Two ":
                                 buttonPanel.setvisible(false);
+                                buttonPanel2.setvisble(true);
                                 break;
                         case " Three ":
                                 system.exit();
                                 break;
-
                         default:
                                 JOptionPane.showMessageDialog( this, "WHERE ARE YOU! THIS DOESNT EXIST!OH GOD HOW DID YOU GET HERE" );
                                 break;
                 }//end of switch
         }//end of Action performed e
                 
-                public void  actionPerformed( SctionEvent f ){
+        public void  actionPerformed( ActionEvent f ){
                 JButton button1 = (JButton)f.getSource();
                 
                 //button actions 
                 switch(button1.getActionCommand() ) {
                         case " 1 ":
-                                EnemyHP-3;
+                                EnemyHP-x;
+                                YourHP-x;
                                 buttonPanel1.setvisible(false);
                                 buttonPanel.setvisible(true);
                                 break;
                         case " 2 ":
-                                EnemyHP-4;
+                                EnemyHP-x;
+                                YourHP-x;
                                 buttonPanel1.setvisible(false);
                                 buttonPanel.setvisible(true);
                                 break;
@@ -127,8 +184,50 @@ public class Test extends JFrame implements ActionListener {
                                 break;
                 }//end of switch
         }//end of action performed f
+        
+        
+        
+        
+        
+        
+        
+        public void  actionPerformed( ActionEvent g ){
+                JButton button2 = (JButton)g.getSource();
+                
+                //button actions 
+                switch(button2.getActionCommand() ) {
+                        case " a ":
+                                EnemyHP-x;
+                                YourHP-x;
+                                buttonPanel2.setvisible(false);
+                                buttonPanel.setvisible(true);
+                                break;
+                        case " b ":
+                                EnemyHP-x;
+                                YourHP-x;
+                                buttonPanel2.setvisible(false);
+                                buttonPanel.setvisible(true);
+                                break;
+                        case " c ":
+                                buttonPanel2.setvisible(false);
+                                buttonPanel.setvisible(true);
+                                break;
+
+                        default:
+                                JOptionPane.showMessageDialog( this, "WHERE ARE YOU! THIS DOESNT EXIST!OH GOD HOW DID YOU GET HERE" );
+                                break;
+                }//end of switch
+        }//end of action performed g
 
 public static void main(Strings[]args)
 {
-        new layoutTest();
+        new.layoutTest();
+        if(EnemyHP<= 0)
+        {
+                JOptionPane.showMessageDialog( this, "YOU WIN");    
+        }//if win
+        if(YourHP<=0)
+        {
+                JOptionPane.showMessageDialog( this, "YOU LOSE");
+        }//if lose
 }//end of main
